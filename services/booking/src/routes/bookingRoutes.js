2b777e598f54bbea4bb7d7872355ae294e6b128a
Amit Saha <2105049@ugrad.cse.buet.ac.bd>
2024-10-25 01:42:08 +0600

    communication done

--- services/booking/src/routes/bookingRoutes.js
+++ services/booking/src/routes/bookingRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { bookTicket } = require('../controllers/bookingController');
+const { bookTicket,bookTicket2 } = require('../controllers/bookingController');
 const router = express.Router();
 
 // Route to book a ticket
-router.post('/book-ticket', bookTicket);
+router.post('/book-ticket', bookTicket2);
 
 router.get('/health', (req, res) => {
     res.status(200).send('Booking service is up and running');
